f7a1e453dfba2b2d4151fdd786c6a18b9dbedbe5
Alberto Spadoni <spadoni@flashstart.com>
2024-10-06 12:36:56 +0200

    fix: now the button correctly redirects the user to the dashboard

--- app/components/GoToDashboard.tsx
+++ app/components/GoToDashboard.tsx
@@ -5,6 +5,8 @@ import { Button } from "@/components/ui/button";
 export const GoToDashboard = () => {
   const router = useRouter();
   return (
-    <Button onClick={() => router.push("/dashboard")}>Go to dashboard</Button>
+    <Button onClick={() => router.push("/dashboard")}>
+      Dashboard
+    </Button>
   );
 };
