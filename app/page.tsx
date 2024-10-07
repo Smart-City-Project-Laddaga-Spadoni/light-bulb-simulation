99c6f361f8f5697d6666e1b7136e614777b8898c
Alberto Spadoni <spadoni@flashstart.com>
2024-10-07 19:31:24 +0200

    feat: application tries to connect to the mqtt broker on startup

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,17 @@
 import BulbsChooser from "@/components/BulbsChooser";
+import { getMqttClient } from "@/lib/mqttClient";
 
-export default function Home() {
+export default async function Home() {
+  const mqttClient = await getMqttClient();
+
   return (
-    <div
-      className="flex flex-col items-center justify-items-center min-h-screen p-8 pb-20 gap-6 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="flex flex-col items-center justify-items-center min-h-screen p-8 pb-20 gap-6 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <h1 className="text-4xl font-bold text-center">Bulbs simulator</h1>
-      <BulbsChooser />
+      {mqttClient && mqttClient.connected ? (
+        <BulbsChooser />
+      ) : (
+        <p>MQTT broker offline. Reload the page to retry...</p>
+      )}
     </div>
   );
 }
