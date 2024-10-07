ca05ea66a2c6465f82452a0e1c2e9331d748d7b7
Alberto Spadoni <spadoni@flashstart.com>
2024-10-07 19:32:06 +0200

    feat: implemented websocket communication between client components and server components

--- app/components/Bulb.tsx
+++ app/components/Bulb.tsx
@@ -1,27 +1,50 @@
 "use client";
-import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import Image from "next/image";
 
 import { useState } from "react";
 import { Button } from "@/components/ui/button";
 import { Slider } from "@/components/ui/slider";
 import { Label } from "@/components/ui/label";
+import { socket } from "@/lib/socket";
 
 enum BulbStates {
   OFF,
-  ON
+  ON,
 }
+
+const publishNewState = (
+  event: string,
+  data: { name: string; newState: number },
+) => {
+  if (socket.connected) socket.emit(event, data);
+};
 
 const Bulb = ({ name }: { name: string }) => {
   const [bulbState, setBulbState] = useState(BulbStates.OFF);
   const [brightness, setBrightness] = useState([50]);
 
   const handleToggle = () => {
-    setBulbState((state) => (state === BulbStates.ON ? BulbStates.OFF : BulbStates.ON));
+    const newState =
+      bulbState === BulbStates.ON ? BulbStates.OFF : BulbStates.ON;
+    setBulbState(newState);
+    if (socket.connected)
+      socket.emit("bulb:state", { bulbName: name, newState });
   };
 
-  const handleBrightness = (value: number[]) => {
-    setBrightness(value);
+  const handleBrightness = (brightness: number[]) => {
+    setBrightness(brightness);
+    if (socket.connected)
+      socket.emit("bulb:brightness", {
+        bulbName: name,
+        newBrightness: brightness[0],
+      });
   };
 
   return (
@@ -39,9 +62,17 @@ const Bulb = ({ name }: { name: string }) => {
       </CardContent>
       <CardFooter className="justify-center">
         <div className="flex flex-col items-center gap-4 w-full">
-          <Button onClick={handleToggle}>Turn {bulbState === BulbStates.ON ? "OFF" : "ON"}</Button>
+          <Button onClick={handleToggle}>
+            Turn {bulbState === BulbStates.ON ? "OFF" : "ON"}
+          </Button>
           <div className="w-full flex justify-between">
-            <Slider disabled={!bulbState} max={100} step={1} value={brightness} onValueChange={handleBrightness} />
+            <Slider
+              disabled={!bulbState}
+              max={100}
+              step={1}
+              value={brightness}
+              onValueChange={handleBrightness}
+            />
             <Label>{brightness[0]}</Label>
           </div>
         </div>
---
+++ server.mjs
@@ -0,0 +1,42 @@
+import { createServer } from "node:http";
+import next from "next";
+import { Server } from "socket.io";
+
+const dev = process.env.NODE_ENV !== "production";
+const hostname = "localhost";
+const port = 3000;
+const app = next({ dev, hostname, port });
+const handler = app.getRequestHandler();
+
+app.prepare().then(() => {
+  const httpServer = createServer(handler);
+
+  const io = new Server(httpServer);
+
+  io.on("connection", (socket) => {
+    console.log("A user connected");
+
+    socket.on("disconnect", () => {
+      console.log("A user disconnected");
+    });
+
+    socket.on("bulb:state", ({ bulbName, newState }) => {
+      console.log(`The ${bulbName} is now ${newState === 1 ? "on" : "off"}`);
+      // publish with mqtt
+    });
+
+    socket.on("bulb:brightness", ({ bulbName, newBrightness }) => {
+      console.log(`The ${bulbName} has changed the brightness to value ${newBrightness}`);
+      // publish with mqtt
+    });
+  });
+
+  httpServer
+    .once("error", (err) => {
+      console.error(err);
+      process.exit(1);
+    })
+    .listen(port, () => {
+      console.log(`> Ready on http://${hostname}:${port}`);
+    });
+});
